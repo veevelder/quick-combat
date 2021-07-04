31d6d7857be518974b675a91a59cf4d7dfd28f30
matt <veevelder@gmail.com>
2021-07-04 12:42:17 -0600

    fixing delete combat tokens

--- quick-combat.js
+++ quick-combat.js
@@ -1,96 +1,94 @@
 import {KeyBinding} from '../settings-extender/settings-extender.js'
 
-
-const registerSettings = () => {
-	// module settings
-	game.settings.register("quick-combat", "playlist", {
-		name: "QuickCombat.Playlist",
-		hint: "QuickCombat.PlaylistHint",
-		scope: "world",
-		config: true,
-		default: 0,
-		isSelect: true,
-		choices: ["None"].concat(game.playlists.contents.map(x => x.data.name)),
-		type: String
-	});
-	
-	game.settings.register("quick-combat", "npcroll", {
-		name: "QuickCombat.NPCRoll",
-		hint: "QuickCombat.NPCRollHint",
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean
-	});
-	
-	var def = false;
-	var conf = false;
-	if (CONFIG.hasOwnProperty("DND5E")) {
-		def = true;
-		conf = true;
-	}
-	game.settings.register("quick-combat", "exp", {
-		name: "QuickCombat.Exp",
-		hint: "QuickCombat.ExpHint",
-		scope: "world",
-		config: conf,
-		default: def,
-		type: Boolean
-	});
-	
-	game.settings.register("quick-combat", "expgm", {
-		name: "QuickCombat.ExpGM",
-		hint: "QuickCombat.ExpGMHint",
-		scope: "world",
-		config: conf,
-		default: false,
-		type: Boolean
-	});
-	
-	game.settings.register("quick-combat", "key", {
-		name: "QuickCombat.Keybind",
-		hint: "QuickCombat.KeybindHint",
-		scope: "world",
-		config: true,
-		default: "Shift + C",
-		type: KeyBinding,
-	});
-	
-	game.settings.register("quick-combat", "rmDefeated", {
-		name: "QuickCombat.RemoveDefeated",
-		hint: "QuickCombat.RemoveDefeatedHint",
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean,
-	});
-	
-	game.settings.register("quick-combat", "oldPlaylist", {
-		scope: "world",
-		config: false,
-		default: "",
-		type: Object
-	});
-	
-	game.settings.register("quick-combat", "inCombat", {
-		scope: "world",
-		config: false,
-		default: false,
-		type: Boolean
-	});
-};
-
 class QuickCombat {
 	static init() {
-		registerSettings();
+		console.debug("quick-combat | register settings")
+		// module settings
+		game.settings.register("quick-combat", "playlist", {
+			name: "QuickCombat.Playlist",
+			hint: "QuickCombat.PlaylistHint",
+			scope: "world",
+			config: true,
+			default: 0,
+			isSelect: true,
+			choices: ["None"].concat(game.playlists.contents.map(x => x.data.name)),
+			type: String
+		});
+
+		game.settings.register("quick-combat", "npcroll", {
+			name: "QuickCombat.NPCRoll",
+			hint: "QuickCombat.NPCRollHint",
+			scope: "world",
+			config: true,
+			default: false,
+			type: Boolean
+		});
+
+		var def = false;
+		var conf = false;
+		if (CONFIG.hasOwnProperty("DND5E")) {
+			def = true;
+			conf = true;
+		}
+		game.settings.register("quick-combat", "exp", {
+			name: "QuickCombat.Exp",
+			hint: "QuickCombat.ExpHint",
+			scope: "world",
+			config: conf,
+			default: def,
+			type: Boolean
+		});
+
+		game.settings.register("quick-combat", "expgm", {
+			name: "QuickCombat.ExpGM",
+			hint: "QuickCombat.ExpGMHint",
+			scope: "world",
+			config: conf,
+			default: false,
+			type: Boolean
+		});
+
+		game.settings.register("quick-combat", "key", {
+			name: "QuickCombat.Keybind",
+			hint: "QuickCombat.KeybindHint",
+			scope: "world",
+			config: true,
+			default: "Shift + C",
+			type: KeyBinding,
+		});
+
+		game.settings.register("quick-combat", "rmDefeated", {
+			name: "QuickCombat.RemoveDefeated",
+			hint: "QuickCombat.RemoveDefeatedHint",
+			scope: "world",
+			config: true,
+			default: false,
+			type: Boolean,
+		});
+
+		game.settings.register("quick-combat", "oldPlaylist", {
+			scope: "world",
+			config: false,
+			default: "",
+			type: Object
+		});
+
+		game.settings.register("quick-combat", "inCombat", {
+			scope: "world",
+			config: false,
+			default: false,
+			type: Boolean
+		});
 	}
 
 	static async rollInitiatives(combat) {
+		console.log("quick-combat | rolling initiatives for NPCs")
 		await combat.rollNPC()
 		//check for PC roll option
 		if (game.settings.get("quick-combat", "npcroll")) {
 			return;
 		}
+		console.log("quick-combat | rolling initiatives for PCs")
 		//roll all PCs that haven't rolled initiative yet
 		await combat.rollInitiative(combat.combatants.filter(a => a.actor.hasPlayerOwner).filter(a => !a.initiative).map(a => a.id))
 	}
@@ -100,6 +98,7 @@ class QuickCombat {
 		var combat = ui.combat.combat;
 		if ( !combat ) {
 			if ( game.user.isGM ) {
+				console.debug("quick-combat | creating new combat")
 				combat = await game.combats.documentClass.create({scene: canvas.scene.id, active: true});
 			}
 			else {
@@ -107,6 +106,7 @@ class QuickCombat {
 			}
 		}
 
+		console.debug("quick-combat | getting player tokens skipping Pets")
 		var tokens = canvas.tokens.controlled.filter(t => t.inCombat === false).filter(function(token) {
 			if (token.actor.data.items.filter(c => c.name == "Pet").length == 0) {
 				return token
@@ -115,8 +115,10 @@ class QuickCombat {
 		
 		// Process each controlled token, as well as the reference token
 		const createData = tokens.map(t => {return {tokenId: t.id, hidden: t.data.hidden}});
+		console.debug("quick-combat | adding combatants to combat")
 		await combat.createEmbeddedDocuments("Combatant", createData)
 		await QuickCombat.rollInitiatives(combat);
+		console.log("quick-combat | starting combat")
 		await combat.startCombat();
 	}
 	
@@ -127,13 +129,10 @@ class QuickCombat {
 			exp += a.actor.data.data.details.xp.value;
 			defeated.push(a.name); 
 		});
-		console.log("exp")
-		console.log(exp)
-		console.log("defeated")
-		console.log(defeated)
 		let pcs = combat.combatants.filter(x => x.actor.hasPlayerOwner);
 		exp = Math.round(exp / pcs.length);
 
+		console.log(`quick-combat | awarding exp ${exp} to PCs`)
 		if (exp != 0 && !isNaN(exp)) {
 			let actor_exp_msg = "<table>";
 			pcs.forEach(function(a) {
@@ -169,34 +168,45 @@ class QuickCombat {
 				}, {});
 			}
 		}
+		else {
+			console.info("quick-combat | no exp for PCs")
+		}
 	}
 	
 	static startPlaylist() {
 		var playlists = game.playlists.playing.map(a => a.data.name);
 		game.settings.set("quick-combat", "oldPlaylist", playlists)
+		console.debug("quick-combat | stopping old playlist ${playlists}")
 		game.playlists.playing.forEach(function(playing) {
 			playing.stopAll()
 		});
 
 		var name = game.settings.settings.get("quick-combat.playlist").choices[game.settings.get("quick-combat", "playlist")]
+		console.debug("quick-combat | starting combat playlist ${name}")
 		game.playlists.getName(name).playAll();
 	}
 	
 	static stopPlaylist() {
 		var name = game.settings.settings.get("quick-combat.playlist").choices[game.settings.get("quick-combat", "playlist")]
+		console.debug("quick-combat | stopping combat playlist ${name}")
 		game.playlists.getName(name).stopAll();
 
-		game.settings.get("quick-combat", "oldPlaylist").forEach(function(playlist) {
+		var playlists = game.settings.get("quick-combat", "oldPlaylist");
+		console.debug("quick-combat | starting old playlist ${playlists}")
+		playlists.forEach(function(playlist) {
 			game.playlists.getName(playlist).playAll();
 		})
 		game.settings.set("quick-combat", "oldPlaylist", [])
 	}
 	
 	static async removeDefeated(combat, userId) {
-		combat.combatants.filter(x => !x.actor.hasPlayerOwner).filter(x => x.defeated).forEach(function(a) {
-			let scene = game.scenes.active;
-			scene.deleteEmbeddedEntity("Token", a.token.id)
+		console.debug("quick-combat | removing defeated NPCs")
+		var ids = []
+		combat.combatants.filter(x => !x.actor.hasPlayerOwner).filter(x => x.data.defeated).forEach(function(a) {
+			ids.push(a.token.id)
 		});
+		let scene = game.scenes.active;
+		await scene.deleteEmbeddedDocuments("Token", ids)
 	}
 }
 
@@ -205,6 +215,7 @@ Hooks.once("ready", function() {
 	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
 		return true;
 
+	console.debug("quick-combat | initializing")
 	window.addEventListener("keydown", ev => {
 		//only allow for non repeat keys on the body by the GM
 		if (ev.repeat || document.activeElement.tagName !== "BODY" || !game.users.filter(a => a.id == game.userId)[0].isGM)
@@ -252,6 +263,7 @@ Hooks.on("ready", function () {
 Hooks.on("preDeleteCombat", (combat, options, userId) => {
 	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
 		return true;
+	console.debug("quick-combat | triggering end of combat functions")
 	game.settings.set("quick-combat", "inCombat", false);
 	if (game.settings.get("quick-combat", "exp")) {
 		QuickCombat.awardExp(combat, userId);
@@ -264,6 +276,7 @@ Hooks.on("preDeleteCombat", (combat, options, userId) => {
 Hooks.on("preUpdateCombat", (combat, route, options, userId) => {
 	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
 		return true;
+	console.debug("quick-combat | triggering start combat functions")
 	if (!game.settings.get("quick-combat", "inCombat")) {
 		game.settings.set("quick-combat", "inCombat", true);
 		if (game.settings.get("quick-combat", "playlist") != 0) {
@@ -275,6 +288,7 @@ Hooks.on("preUpdateCombat", (combat, route, options, userId) => {
 Hooks.on("deleteCombat", (combat, options, userId) => {
 	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
 		return true;
+	console.debug("quick-combat | triggering delete combatant functions")
 	if (game.settings.get("quick-combat", "rmDefeated")) {
 		QuickCombat.removeDefeated(combat, userId);
 	}
