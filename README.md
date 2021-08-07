f6ebf191daad60ac975b85799b2a0ff5259fdc01
matt <veevelder@gmail.com>
2021-08-06 18:23:51 -0600

    added fanfare feature

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Adds Auto Experience Tracking. When NPCs are defeated it will add the experience
 
 Other Systems are planned for experience tracking support in the future, as of now DND5E is the only system that is supported. 
 
-Adds two Hotkeys to quickly add PCs, and NPCs to the combat tracker, roll their initiatives, and start combat. The other hotkey will do the same but not start the combat playlist.
+Adds a Hotkey to quickly add PCs, and NPCs to the combat tracker, roll their initiatives, and start combat.
 
 This module has been tested with the D&D 5E system. Plan is to make it available to other systems in the future. For other systems, everything except the Experience tracking should work, but this has not been tested yet.
 
@@ -18,11 +18,12 @@ This module has been tested with the D&D 5E system. Plan is to make it available
 ![Quick Combat Settings](images/settings.png)
 
 * **Combat Playlist** - select from a list of playlists to play when starting combat, or None to not change the playlist.
+* **Fanfare Playlist** - select from a list of playlists to play when combat ends, will randomly select a track to play.
 * **Only Roll Initiative for NPCs?** - When adding combatants to the combat tracker, only roll for NPCs and not PCs. If your players like to roll their own initiatives.
 * **Combat Experience Tracking** - (DND5E Only!) For any defeated NPCs will add any experience and for all PCs in combat will add any gained experience.
 * **GM Experience Whisper** - Only Message the GM the experience gained.
 * **Combat Toggle Keybind** - Keybind combination to start and stop combat.
-* **Combat Toggle Keybind Alt** - Keybind combination to start and stop combat, without starting the Combat Playlist.
+* **Combat Toggle Keybind WITHOUT playlist** - Keybind combination to start and stop combat, but will not start the Combat Playlist or Fanfare Playlist.
 * **Remove Defeated NPCs?** - Will delete any of the defeated NPC tokens from the scene.
 
 ### Start Combat
@@ -30,7 +31,7 @@ This module has been tested with the D&D 5E system. Plan is to make it available
 
 Select all tokens for combat.
 
-Hit the Combat Toggle Keybind or Alt Toggle Keybind hot key.
+Hit the Combat Toggle Keybind hot key.
 
 ![Quick Combat Start](images/combat-start.png)
 
@@ -39,9 +40,9 @@ Adds all selected tokens to combat tracker, rolls all tokens initiatives, starts
 ### End Combat
 ![Quick Combat End](images/end-combat-warning.png)
 
-Hit the Combat Toggle Keybind or the Alt Toggle Keybind hot key, a warning will pop up to make sure it wasn't an accident.
+Hit the Combat Toggle Keybind hot key, a warning will pop up to make sure it wasn't an accident.
 
-Will remove all combatants, and if experience tracking was enabled will find all defeated NPCs, calculate experience and display a chat box with all PCs experience.
+Will remove all combatants, and if experience tracking was enabled will find all defeated NPCs, calcuate experience and display a chat box with all PCs experience.
 
 ![Quick End Experience](images/experience-tracking.png)
 
@@ -58,6 +59,14 @@ Start Combat.
 
 Stop Combat and will start the "Background" playlist again.
 
+### Fanfare Playlist
+Fanfare Playlist will only play at the end of combat. It will randomly select a track and play it once. After the track is done playing the playlist that was playing before combat started will start again.
+
+![Fanfare Settings](images/fanfare_settings.png)
+
+It is recommended that you set up the playlist using the `Soundboard Only` Playback Mode as shown below. You can also set the Fade Duration for any track if you require it.
+
+![Fanfare Playlist](images/fanfare_playlist.png)
 
 ### NPC Rolling Only
 ![Quick Combat NPC Rolling](images/npc-rolls.png)
@@ -66,19 +75,3 @@ If set up will only roll for NPC combatants and not PCs.
 
 ![Quick Combat NPC Rolls](images/npc-roll.png)
 
-
-### GM Experience Whisper
-![Quick Combat Settings GM EXP Whisper](images/settings-exp-whisper.png)
-
-If enabled it will only send the experience chat message to the GM, but will still distribute experience to PCs.
-
-![Quick Combat GM EXP Whisper](images/exp-whisper.png)
-
-
-### Remove Defeated NPCs?
-![Quick Combat Settings Remove Defeated](images/settings-remove-defeated.png)
-
-If enabled it will delete any defeated NPC tokens from the scene.
-
-![Quick Combat Remove Defeated Before](images/removed-defeated-before.png)
-![Quick Combat Remove Defeated After](images/removed-defeated-after.png)
--- quick-combat.js
+++ quick-combat.js
@@ -2,131 +2,228 @@ import {KeyBinding} from '../settings-extender/settings-extender.js'
 
 var skipPlaylist = false;
 
-class QuickCombat {
-	static init() {
-		console.debug("quick-combat | register settings")
-		// module settings
-		game.settings.register("quick-combat", "playlist", {
-			name: "QuickCombat.Playlist",
-			hint: "QuickCombat.PlaylistHint",
-			scope: "world",
-			config: true,
-			default: 0,
-			isSelect: true,
-			choices: ["None"].concat(game.playlists.contents.map(x => x.data.name)),
-			type: String
-		});
+Hooks.once("ready", function() {
+	//only allow for the GM
+	if (!game.user.isGM)
+		return true;
 
-		game.settings.register("quick-combat", "npcroll", {
-			name: "QuickCombat.NPCRoll",
-			hint: "QuickCombat.NPCRollHint",
-			scope: "world",
-			config: true,
-			default: false,
-			type: Boolean
-		});
+	console.debug("quick-combat | initializing")
+	window.addEventListener("keydown", async ev => {
+		//only allow for non repeat keys on the body by the GM
+		if (ev.repeat || document.activeElement.tagName !== "BODY" || !game.users.filter(a => a.id == game.userId)[0].isGM)
+			return true;
 
-		var def = false;
-		var conf = false;
-		if (CONFIG.hasOwnProperty("DND5E")) {
-			def = true;
-			conf = true;
+		let start_combat = false;
+		let setting_alt = game.settings.get("quick-combat", "keyalt")
+		if (setting_alt != null) {
+			const keyalt = KeyBinding.parse(setting_alt)
+			if (KeyBinding.eventIsForBinding(ev, keyalt)) {
+				start_combat = true;
+				skipPlaylist = true;
+				console.debug("quick-combat | alt hotkey pressed")
+			}
 		}
-		game.settings.register("quick-combat", "exp", {
-			name: "QuickCombat.Exp",
-			hint: "QuickCombat.ExpHint",
-			scope: "world",
-			config: conf,
-			default: def,
-			type: Boolean
-		});
-
-		game.settings.register("quick-combat", "expgm", {
-			name: "QuickCombat.ExpGM",
-			hint: "QuickCombat.ExpGMHint",
-			scope: "world",
-			config: conf,
-			default: false,
-			type: Boolean
-		});
-
-		game.settings.register("quick-combat", "key", {
-			name: "QuickCombat.Keybind",
-			hint: "QuickCombat.KeybindHint",
-			scope: "world",
-			config: true,
-			default: "Shift + C",
-			type: KeyBinding,
-		});
-
-		game.settings.register("quick-combat", "keyalt", {
-			name: "QuickCombat.KeybindAlt",
-			hint: "QuickCombat.KeybindAltHint",
-			scope: "world",
-			config: true,
-			default: "Shift + Alt + C",
-			type: KeyBinding,
-		});
-
-		game.settings.register("quick-combat", "rmDefeated", {
-			name: "QuickCombat.RemoveDefeated",
-			hint: "QuickCombat.RemoveDefeatedHint",
-			scope: "world",
-			config: true,
-			default: false,
-			type: Boolean,
-		});
-
-		game.settings.register("quick-combat", "oldPlaylist", {
-			scope: "world",
-			config: false,
-			default: "",
-			type: Object
-		});
-	}
-
-	static async rollInitiatives(combat) {
-		console.log("quick-combat | rolling initiatives for NPCs")
-		await combat.rollNPC()
-		//check for PC roll option
-		if (game.settings.get("quick-combat", "npcroll")) {
-			return;
+		let setting_key = game.settings.get("quick-combat", "key")
+		if (setting_key != null && !start_combat) {
+			const key = KeyBinding.parse(setting_key)
+			if (KeyBinding.eventIsForBinding(ev, key)) {
+				start_combat = true;
+				//skipPlaylist = false;
+				console.debug("quick-combat | hotkey pressed")
+			}
 		}
-		console.log("quick-combat | rolling initiatives for PCs")
-		//roll all PCs that haven't rolled initiative yet
-		await combat.rollInitiative(combat.combatants.filter(a => a.actor.hasPlayerOwner).filter(a => !a.initiative).map(a => a.id))
-	}
-	
-	static async addCombatants() {
-		// Reference the combat encounter displayed in the Sidebar if none was provided
-		var combat = ui.combat.combat;
-		if ( !combat ) {
-			if ( game.user.isGM ) {
-				console.debug("quick-combat | creating new combat")
-				combat = await game.combats.documentClass.create({scene: canvas.scene.id, active: true});
+
+		if (start_combat) {
+			console.debug(`quick-combat | skip playlist? ${skipPlaylist}`)
+			ev.preventDefault();
+			ev.stopPropagation();
+			if (game.combat) {
+				console.debug("quick-combat | combat found stopping combat")
+				game.combat.endCombat();
 			}
 			else {
-				return ui.notifications.warn(game.i18n.localize("COMBAT.NoneActive"));
+				console.debug("quick-combat | no combat found starting combat")
+				//check if combat tracker has combatants
+				if(game.combat && game.combat.combatants.length > 0) {
+					game.combat.startCombat();
+				}
+				//check if GM has any selected tokens
+				else if (canvas.tokens.controlled.length === 0) {
+					ui.notifications.error(game.i18n.localize("QuickCombat.KeyError"));
+				}
+				else {
+					// Reference the combat encounter displayed in the Sidebar if none was provided
+					var combat = ui.combat.combat;
+					if ( !combat ) {
+						if ( game.user.isGM ) {
+							console.debug("quick-combat | creating new combat")
+							combat = await game.combats.documentClass.create({scene: canvas.scene.id, active: true});
+						}
+						else {
+							return ui.notifications.warn(game.i18n.localize("COMBAT.NoneActive"));
+						}
+					}
+					else {
+						combat = game.combat;
+					}
+
+					console.debug("quick-combat | getting player tokens skipping Pets")
+					var tokens = canvas.tokens.controlled.filter(t => t.inCombat === false).filter(function(token) {
+						if (token.actor.data.items.filter(c => c.name == "Pet").length == 0) {
+							return token
+						}
+					});
+					
+					// Process each controlled token, as well as the reference token
+					const createData = tokens.map(t => {return {tokenId: t.id, hidden: t.data.hidden}});
+					console.debug("quick-combat | adding combatants to combat")
+					await combat.createEmbeddedDocuments("Combatant", createData)
+					console.log("quick-combat | rolling initiatives for NPCs")
+					await combat.rollNPC()
+					//check for PC roll option
+					if (game.settings.get("quick-combat", "npcroll")) {
+						return;
+					}
+					console.log("quick-combat | rolling initiatives for PCs")
+					//roll all PCs that haven't rolled initiative yet
+					await combat.rollInitiative(combat.combatants.filter(a => a.actor.hasPlayerOwner).filter(a => !a.initiative).map(a => a.id))
+					console.log("quick-combat | starting combat")
+					await combat.startCombat();
+				}
 			}
 		}
+	});
+});
 
-		console.debug("quick-combat | getting player tokens skipping Pets")
-		var tokens = canvas.tokens.controlled.filter(t => t.inCombat === false).filter(function(token) {
-			if (token.actor.data.items.filter(c => c.name == "Pet").length == 0) {
-				return token
-			}
-		});
-		
-		// Process each controlled token, as well as the reference token
-		const createData = tokens.map(t => {return {tokenId: t.id, hidden: t.data.hidden}});
-		console.debug("quick-combat | adding combatants to combat")
-		await combat.createEmbeddedDocuments("Combatant", createData)
-		await QuickCombat.rollInitiatives(combat);
-		console.log("quick-combat | starting combat")
-		await combat.startCombat();
+Hooks.on("ready", () => {
+	if (!game.user.isGM)
+		return true;
+
+	console.debug("quick-combat | register settings")
+	// module settings
+	game.settings.register("quick-combat", "playlist", {
+		name: "QuickCombat.Playlist",
+		hint: "QuickCombat.PlaylistHint",
+		scope: "world",
+		config: true,
+		default: 0,
+		isSelect: true,
+		choices: ["None"].concat(game.playlists.contents.map(x => x.data.name)),
+		type: String
+	});
+
+	game.settings.register("quick-combat", "fanfare-playlist", {
+		name: "QuickCombat.FanfarePlaylist",
+		hint: "QuickCombat.FanfarePlaylistHint",
+		scope: "world",
+		config: true,
+		default: 0,
+		isSelect: true,
+		choices: ["None"].concat(game.playlists.contents.map(x => x.data.name)),
+		type: String
+	});
+
+	game.settings.register("quick-combat", "npcroll", {
+		name: "QuickCombat.NPCRoll",
+		hint: "QuickCombat.NPCRollHint",
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
+
+	var def = false;
+	var conf = false;
+	if (CONFIG.hasOwnProperty("DND5E")) {
+		def = true;
+		conf = true;
 	}
-	
-	static awardExp(combat, userId) {
+	game.settings.register("quick-combat", "exp", {
+		name: "QuickCombat.Exp",
+		hint: "QuickCombat.ExpHint",
+		scope: "world",
+		config: conf,
+		default: def,
+		type: Boolean
+	});
+
+	game.settings.register("quick-combat", "expgm", {
+		name: "QuickCombat.ExpGM",
+		hint: "QuickCombat.ExpGMHint",
+		scope: "world",
+		config: conf,
+		default: false,
+		type: Boolean
+	});
+
+	game.settings.register("quick-combat", "key", {
+		name: "QuickCombat.Keybind",
+		hint: "QuickCombat.KeybindHint",
+		scope: "world",
+		config: true,
+		default: "Shift + C",
+		type: KeyBinding,
+	});
+
+	game.settings.register("quick-combat", "keyalt", {
+		name: "QuickCombat.KeybindAlt",
+		hint: "QuickCombat.KeybindAltHint",
+		scope: "world",
+		config: true,
+		default: "Shift + Alt + C",
+		type: KeyBinding,
+	});
+
+	game.settings.register("quick-combat", "rmDefeated", {
+		name: "QuickCombat.RemoveDefeated",
+		hint: "QuickCombat.RemoveDefeatedHint",
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean,
+	});
+
+	game.settings.register("quick-combat", "oldPlaylist", {
+		scope: "world",
+		config: false,
+		default: "",
+		type: Object
+	});
+});
+
+Hooks.on("preUpdateCombat", async (combat, update, options, userId) => {
+	const combatStart = combat.round === 0 && update.round === 1;
+	if (!game.user.isGM || !combatStart)
+		return true;
+
+	console.debug("quick-combat | triggering start combat functions")
+	let playlist = game.settings.get("quick-combat", "playlist")
+	if (!skipPlaylist && game.settings.get("quick-combat", "playlist") != 0) {
+		console.log("quick-combat | start combat playlist")
+		var playlists = [];
+		game.playlists.playing.forEach(function(playing) {
+			console.debug(`quick-combat | stopping old playlist ${playing.name}`)
+			playlists.push(playing.name)
+			playing.stopAll()
+		});
+		game.settings.set("quick-combat", "oldPlaylist", playlists)
+
+		var name = game.settings.settings.get("quick-combat.playlist").choices[game.settings.get("quick-combat", "playlist")]
+		console.debug(`quick-combat | starting combat playlist ${name}`)
+		await game.playlists.getName(name).playAll();
+	}
+	else {
+		console.warn("No combat playlist was found, skipping")
+	}
+});
+
+
+Hooks.on("deleteCombat", async (combat, options, userId) => {
+	if (!game.user.isGM)
+		return true;
+	console.debug("quick-combat | triggering delete combatant functions")
+	//give exp
+	if (game.settings.get("quick-combat", "exp")) {
 		let exp = 0;
 		let defeated = [];
 		combat.combatants.filter(x => !x.actor.hasPlayerOwner).filter(x => x.data.defeated).forEach(function(a) { 
@@ -181,34 +278,28 @@ class QuickCombat {
 			}
 		}
 	}
-	
-	static startPlaylist() {
-		var playlists = game.playlists.playing.map(a => a.data.name);
-		game.settings.set("quick-combat", "oldPlaylist", playlists)
-		console.debug("quick-combat | stopping old playlist ${playlists}")
-		game.playlists.playing.forEach(function(playing) {
-			playing.stopAll()
-		});
+	//stop combat playlist and play fanfare
+	console.log(skipPlaylist)
+	if (!skipPlaylist && game.settings.get("quick-combat", "playlist") != 0) {
+		//stop combat playlist
+		var name = game.settings.settings.get("quick-combat.playlist").choices[game.settings.get("quick-combat", "playlist")]
+		console.debug(`quick-combat | stopping combat playlist ${name}`)
+		await game.playlists.getName(name).stopAll();
 
-		var name = game.settings.settings.get("quick-combat", "playlist").choices[game.settings.get("quick-combat", "playlist")]
-		console.debug("quick-combat | starting combat playlist ${name}")
-		game.playlists.getName(name).playAll();
+		//play fanfare playlist
+		if (game.settings.get("quick-combat", "fanfare-playlist") != 0) {
+			var fanfare = game.settings.settings.get("quick-combat.fanfare-playlist").choices[game.settings.get("quick-combat", "fanfare-playlist")]
+			if(fanfare) {
+				console.debug(`quick-combat | starting fanfare playlist ${fanfare}`)
+				var items = Array.from(game.playlists.getName(fanfare).data.sounds);
+				var item = items[Math.floor(Math.random()*items.length)];
+				console.debug(`quick-combat | starting fanfare track ${item.name}`)
+				game.playlists.getName(fanfare).playSound(item);
+			}
+		}
 	}
-	
-	static stopPlaylist() {
-		var name = game.settings.settings.get("quick-combat", "playlist").choices[game.settings.get("quick-combat", "playlist")]
-		console.debug("quick-combat | stopping combat playlist ${name}")
-		game.playlists.getName(name).stopAll();
-
-		var playlists = game.settings.get("quick-combat", "oldPlaylist");
-		console.debug("quick-combat | starting old playlist ${playlists}")
-		playlists.forEach(function(playlist) {
-			game.playlists.getName(playlist).playAll();
-		})
-		game.settings.set("quick-combat", "oldPlaylist", [])
-	}
-	
-	static async removeDefeated(combat, userId) {
+	//remove defeated npcs
+	if (game.settings.get("quick-combat", "rmDefeated")) {
 		console.debug("quick-combat | removing defeated NPCs")
 		var ids = []
 		combat.combatants.filter(x => !x.actor.hasPlayerOwner).filter(x => x.data.defeated).forEach(function(a) {
@@ -217,98 +308,40 @@ class QuickCombat {
 		let scene = game.scenes.active;
 		await scene.deleteEmbeddedDocuments("Token", ids)
 	}
-}
 
-Hooks.once("ready", function() {
-	//only allow for the GM
-	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
-		return true;
-
-	console.debug("quick-combat | initializing")
-	window.addEventListener("keydown", ev => {
-		//only allow for non repeat keys on the body by the GM
-		if (ev.repeat || document.activeElement.tagName !== "BODY" || !game.users.filter(a => a.id == game.userId)[0].isGM)
-			return true;
-
-		let start_combat = false;
-		let setting_alt = game.settings.get("quick-combat", "keyalt")
-		if (setting_alt != null) {
-			const keyalt = KeyBinding.parse(setting_alt)
-			if (KeyBinding.eventIsForBinding(ev, keyalt)) {
-				start_combat = true;
-				skipPlaylist = true;
-				console.debug("quick-combat | alt hotkey pressed")
-			}
-		}
-		let setting_key = game.settings.get("quick-combat", "key")
-		if (setting_key != null && !start_combat) {
-			const key = KeyBinding.parse(setting_key)
-			if (KeyBinding.eventIsForBinding(ev, key)) {
-				start_combat = true;
-				skipPlaylist = false;
-				console.debug("quick-combat | hotkey pressed")
-			}
-		}
-
-		if (start_combat) {
-			console.debug(`quick-combat | skip playlist? ${skipPlaylist}`)
-			ev.preventDefault();
-			ev.stopPropagation();
-			if (game.combat != null) {
-				game.combat.endCombat();
-			}
-			else {
-				//check if combat tracker has combatants
-				if(combat.combatants && combat.combatants.length > 0) {
-					game.combat.startCombat();
-				}
-				//check if GM has any selected tokens
-				else if (canvas.tokens.controlled.length === 0) {
-					ui.notifications.error(game.i18n.localize("QuickCombat.KeyError"));
-				}
-				else {
-					QuickCombat.addCombatants();
-				}
-			}
-		}
-	});
-});
-
-Hooks.on("ready", function () {
-	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
-		return true;
-	QuickCombat.init();
-});
-
-Hooks.on("preDeleteCombat", (combat, options, userId) => {
-	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
-		return true;
-	console.debug("quick-combat | triggering end of combat functions")
+	//reset skip playlist
 	skipPlaylist = false;
-	if (game.settings.get("quick-combat", "exp")) {
-		QuickCombat.awardExp(combat, userId);
-	}
-	if (game.settings.get("quick-combat", "playlist") != 0) {
-		QuickCombat.stopPlaylist();
-	}
 });
 
-Hooks.on("preUpdateCombat", (combat, route, options, userId) => {
-	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
+Hooks.on("updatePlaylist", async (playlist, update, options, userId) => {
+	//only run for the GM
+	if (!game.user.isGM)
 		return true;
-	console.debug("quick-combat | triggering start combat functions")
-	let playlist = game.settings.get("quick-combat", "playlist")
-	if (!skipPlaylist && game.settings.get("quick-combat", "playlist") != 0) {
-		console.log("quick-combat | start combat playlist")
-		QuickCombat.startPlaylist();
+	//dont do anything if the update is set to playing
+	if (update.playing)
+		return true;
+
+	//if fanfare playlist has been set
+	if (game.settings.get("quick-combat", "fanfare-playlist") != 0) {
+		var fanfare = game.settings.settings.get("quick-combat.fanfare-playlist").choices[game.settings.get("quick-combat", "fanfare-playlist")]
+		if (playlist.data.name != fanfare)
+			return true;
 	}
+	//otherwise check if combat playlist has stopped
+	else {
+		var name = game.settings.settings.get("quick-combat.playlist").choices[game.settings.get("quick-combat", "playlist")]
+		if (playlist.data.name != name)
+			return true;
+	}
+	
+
+	console.debug("quick-combat | starting old playlist")
+	//start old playlist
+	var playlists = game.settings.get("quick-combat", "oldPlaylist");
+	console.debug(`quick-combat | starting old playlist ${playlists}`)
+	playlists.forEach(function(playlist) {
+		game.playlists.getName(playlist).playAll();
+	})
+	game.settings.set("quick-combat", "oldPlaylist", [])
 });
 
-Hooks.on("deleteCombat", (combat, options, userId) => {
-	if (!game.users.filter(a => a.id == game.userId)[0].isGM)
-		return true;
-	console.debug("quick-combat | triggering delete combatant functions")
-	if (game.settings.get("quick-combat", "rmDefeated")) {
-		QuickCombat.removeDefeated(combat, userId);
-	}
-});
